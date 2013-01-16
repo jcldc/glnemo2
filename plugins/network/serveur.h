f3a094bd867325d0e694813d8c89556e2b6dad6a
jclamber <jclamber@6a5dd85c-8a21-4469-84b3-770a23de0f29>
2013-01-16 10:17:00 +0000

    update copyright to 2013
    git-svn-id: http://svn.lam.fr/repos/glnemo2/trunk@187 6a5dd85c-8a21-4469-84b3-770a23de0f29

--- plugins/network/serveur.h
+++ plugins/network/serveur.h
@@ -1,5 +1,5 @@
 // ============================================================================
-// Copyright Jean-Charles LAMBERT - 2007-2012
+// Copyright Jean-Charles LAMBERT - 2007-2013
 //           Yannick Dalbin
 // e-mail:   Jean-Charles.Lambert@oamp.fr
 // address:  Dynamique des galaxies
