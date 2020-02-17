b3fe3f1d4c6e51f4d61df956abf5fffb839ec84e
LAMBERT Jean-charles <jean-charles.lambert@lam.fr>
2020-02-17 15:11:42 +0100

    strip shared-lib

--- CI/scripts/build.sh
+++ CI/scripts/build.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
+echo "Before striping"
+ldd /usr/lib64/qt5/bin/uic
+
 strip --remove-section=.note.ABI-tag /lib64/libQt5Core.so
+
+echo "After stripping"
+ldd /usr/lib64/qt5/bin/uic
+
 
 cmake .
 make -j 8
