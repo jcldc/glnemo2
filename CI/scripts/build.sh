227bfecabf9e3e1bbc15500e9eafae3a0dd493b8
LAMBERT Jean-charles <jean-charles.lambert@lam.fr>
2020-02-19 13:52:07 +0100

    update stip ABI for ubuntu

--- CI/scripts/build.sh
+++ CI/scripts/build.sh
@@ -3,7 +3,16 @@
 
 # strip qt5 lib as a workaround on unresolved dependency
 # see https://github.com/microsoft/WSL/issues/3023
-strip --remove-section=.note.ABI-tag /usr/lib64/libQt5Core.so
+
+# fedora/opensuse etc...
+if [ -f /usr/lib64/libQt5Core.so ]; then
+    strip --remove-section=.note.ABI-tag /usr/lib64/libQt5Core.so
+fi
+
+# ubuntu
+if [ -f /lib/x86_64-linux-gnu/libQt5Core.so ]; then
+    strip --remove-section=.note.ABI-tag /lib/x86_64-linux-gnu/libQt5Core.so
+fi
 
 cmake .
 make -j 8
