a2e8f9c1670e9b6a3aa236c839d3f2c07871f09a
LAMBERT Jean-charles <jean-charles.lambert@lam.fr>
2022-10-07 15:58:49 +0200

    new macosx machine

--- CI/scripts/buildOSX.sh
+++ CI/scripts/buildOSX.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Build script for MacOSX gitlab runner
 
-cmake . -DCMAKE_PREFIX_PATH=/Users/jcl/Qt2/5.10.0/clang_64/lib/cmake 
+cmake . -DCMAKE_PREFIX_PATH=/Users/grunner/Qt/5.15.2/clang_64/lib/cmake
 make -j 3
 make package
