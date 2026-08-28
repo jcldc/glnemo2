066ee92d940ba52d5e04f2de04c52d8b6487c06e
LAMBERT Jean-charles <jean-charles.lambert@lam.fr>
2026-08-28 15:46:27 +0200

    improve compilation
    CPACK generator more generic for every distros
    Correct cmakefile syntaxes for nvim linting

--- cmake/DetectCpackGenerator.cmake
+++ cmake/DetectCpackGenerator.cmake
@@ -1,6 +1,6 @@
 # -*-cmake-*-
 # ============================================================================
-# Copyright Jean-Charles LAMBERT - 2008-2020
+# Copyright Jean-Charles LAMBERT - 2007-2026
 # e-mail:   Jean-Charles.Lambert@lam.fr
 # address:  Dynamique des galaxies
 #           Centre de donneeS Astrophysique de Marseille (CeSAM)
@@ -13,142 +13,26 @@
 # Detect OS and architecture to select right CPACK Generator
 # ============================================================================
 #
-# Inspired from :
-# http://www.cmake.org/pipermail/cmake/2010-July/038268.html
-# http://www.cmake.org/pipermail/cmake/attachments/20100722/04976a73/attachment-0001.obj
 #
+if(UNIX AND NOT APPLE)
+  # CMake read system info from /etc/os-release
+  cmake_host_system_information(RESULT OS_ID QUERY DISTRIB_ID)
+  cmake_host_system_information(RESULT OS_LIKE QUERY DISTRIB_ID_LIKE)
 
+  string(TOLOWER "${OS_ID}" OS_ID)
+  string(TOLOWER "${OS_LIKE}" OS_LIKE)
 
-
-# define a set of string with may-be useful readable name
-# this file is meant to be included in a CMakeLists.txt
-# not as a standalone CMake script
-set(SPECIFIC_COMPILER_NAME "")
-set(SPECIFIC_SYSTEM_VERSION_NAME "")
-set(SPECIFIC_SYSTEM_PREFERED_CPACK_GENERATOR "")
-set(LINUX_NAME "")
-set(MY_OS "")
-
-if(UNIX)
-  if(CMAKE_SYSTEM_NAME MATCHES "Linux")
-    set(SPECIFIC_SYSTEM_VERSION_NAME "${CMAKE_SYSTEM_NAME}")
-    set(SPECIFIC_SYSTEM_PREFERED_CPACK_GENERATOR "TGZ")
-    set(LINUX_NAME "")
-    set(LINUX_ISSUE "")
-    if(EXISTS "/etc/system-release")
-      file(READ "/etc/system-release" LINUX_ISSUE)
-    elseif (EXISTS "/etc/issue")
-      file(READ "/etc/issue" LINUX_ISSUE)
-    elseif (EXISTS "/etc/redhat-release")
-      file(READ "/etc/redhat-release" LINUX_ISSUE)
-    elseif (EXISTS "/etc/os-release")
-      file(READ "/etc/os-release" LINUX_ISSUE)
-    endif(EXISTS "/etc/system-release")      
-
-    if(EXISTS "/etc/os-release")
-      file(READ "/etc/os-release" LINUX_RELEASE)
-    endif(EXISTS "/etc/os-release")
-
-    # Mageia case
-    if(LINUX_ISSUE MATCHES "Mageia")
-      set(MY_OS "Mageia")
-      string(REGEX MATCH "release ([0-9]+)" MAGEIA "${LINUX_ISSUE}")
-      set(LINUX_NAME "mga${CMAKE_MATCH_1}")  
-      set(SPECIFIC_SYSTEM_PREFERED_CPACK_GENERATOR "RPM")      
-      MESSAGE (STATUS "Mageia :" ${LINUX_NAME})
-    endif(LINUX_ISSUE MATCHES "Mageia")
-    # Arch Linux
-    if(LINUX_ISSUE MATCHES "Arch")
-      set(MY_OS "Arch")
-      set(LINUX_NAME "arch")  
-      #set(SPECIFIC_SYSTEM_PREFERED_CPACK_GENERATOR "RPM")      
-      MESSAGE (STATUS "Arch Linux :" ${LINUX_NAME})
-    endif(LINUX_ISSUE MATCHES "Arch")
-    # Fedora case
-    if(LINUX_ISSUE MATCHES "Fedora")
-      set(MY_OS "Fedora")
-      string(REGEX MATCH "release ([0-9]+)" FEDORA "${LINUX_ISSUE}")
-      set(LINUX_NAME "fc${CMAKE_MATCH_1}")  
-      set(SPECIFIC_SYSTEM_PREFERED_CPACK_GENERATOR "RPM")      
-    endif(LINUX_ISSUE MATCHES "Fedora")
-    # Scientific case
-    if(LINUX_ISSUE MATCHES "Scientific")
-      set(MY_OS "Scientific")
-      string(REGEX MATCH "release ([0-9]+)" SCIENTIFIC "${LINUX_ISSUE}")
-      set(LINUX_NAME "el${CMAKE_MATCH_1}")
-      set(SPECIFIC_SYSTEM_PREFERED_CPACK_GENERATOR "RPM")
-    endif(LINUX_ISSUE MATCHES "Scientific")
-    if(LINUX_ISSUE MATCHES "CentOS")
-      set(MY_OS "CentOS")
-      string(REGEX MATCH "release ([0-9]+)" CENTOS "${LINUX_ISSUE}")
-      set(LINUX_NAME "el${CMAKE_MATCH_1}")
-      set(SPECIFIC_SYSTEM_PREFERED_CPACK_GENERATOR "RPM")
-    endif(LINUX_ISSUE MATCHES "CentOS")
-    # Ubuntu case
-    if(LINUX_RELEASE MATCHES "Ubuntu")
-      set (CPACK_DEBIAN_PACKAGE_SHLIBDEPS ON) 
-      set(MY_OS "Ubuntu")
-      string(REGEX MATCH "([0-9]+\\.[0-9]+)" UBUNTU "${LINUX_RELEASE}")
-      set(LINUX_NAME "ubuntu${CMAKE_MATCH_1}")        
-      set(SPECIFIC_SYSTEM_PREFERED_CPACK_GENERATOR "DEB")
-    endif(LINUX_RELEASE MATCHES "Ubuntu")
-    # Mint case
-    if(LINUX_ISSUE MATCHES "Mint")
-      set (CPACK_DEBIAN_PACKAGE_SHLIBDEPS ON)
-      set(MY_OS "Mint")
-      string(REGEX MATCH "Mint ([0-9]+\\.[0-9]+)" MINT "${LINUX_ISSUE}")
-      set(LINUX_NAME "mint${CMAKE_MATCH_1}")
-      set(SPECIFIC_SYSTEM_PREFERED_CPACK_GENERATOR "DEB")
-    endif(LINUX_ISSUE MATCHES "Mint")
-    # Debian case
-    if(LINUX_ISSUE MATCHES "Debian")
-      set (CPACK_DEBIAN_PACKAGE_SHLIBDEPS ON) 
-      string(REGEX MATCH "Debian .*ux ([a-zA-Z]*/?[a-zA-Z]*) .*" DEBIAN "${LINUX_ISSUE}")
-      set(LINUX_NAME "debian-${CMAKE_MATCH_1}")
-      string(REPLACE "/" "_" LINUX_NAME ${LINUX_NAME}) 
-      set(SPECIFIC_SYSTEM_PREFERED_CPACK_GENERATOR "DEB")       
-    endif(LINUX_ISSUE MATCHES "Debian")      
-    # Open SuSE case
-    if(LINUX_RELEASE MATCHES "Leap")
-      set(MY_OS "OpenSUSE LEAP")
-      string(REGEX MATCH "VERSION=\"([0-9]+\\.[0-9]+)\"" SUSE "${LINUX_RELEASE}")
-      if (NOT ${CMAKE_MATCH_2} STREQUAL "")
-        set(LINUX_NAME "opensuse${CMAKE_MATCH_2}")
-      else()
-        set(LINUX_NAME "opensuse${CMAKE_MATCH_1}")
-      endif()
-      string(REPLACE "/" "_" LINUX_NAME ${LINUX_NAME})   
-      set(SPECIFIC_SYSTEM_PREFERED_CPACK_GENERATOR "RPM")     
-    elseif (LINUX_RELEASE MATCHES "SUSE")
-      set(MY_OS "Suse")
-      string(REGEX MATCH "SUSE ([a-zA-Z]*/?[a-zA-Z]*) ([0-9]+\\.[0-9]+)" SUSE "${LINUX_RELEASE}")
-      if (NOT ${CMAKE_MATCH_2} STREQUAL "")
-        set(LINUX_NAME "opensuse-tumbleweed${CMAKE_MATCH_2}")
-      else()
-        set(LINUX_NAME "opensuse-tumbleweed${CMAKE_MATCH_1}")
-      endif()
-      string(REPLACE "/" "_" LINUX_NAME ${LINUX_NAME})
-      set(SPECIFIC_SYSTEM_PREFERED_CPACK_GENERATOR "RPM")
-    endif()
-    # Mandriva case
-    # TODO      
-    if(LINUX_NAME) 
-      set(SPECIFIC_SYSTEM_VERSION_NAME "${CMAKE_SYSTEM_NAME}-${LINUX_NAME}")
-    endif(LINUX_NAME)    
-  endif(CMAKE_SYSTEM_NAME MATCHES "Linux")
-  set(SPECIFIC_SYSTEM_VERSION_NAME "${SPECIFIC_SYSTEM_VERSION_NAME}-${CMAKE_SYSTEM_PROCESSOR}")
-  set(SPECIFIC_COMPILER_NAME "")
-endif(UNIX)
-
-if    (NOT SPECIFIC_SYSTEM_PREFERED_CPACK_GENERATOR STREQUAL "" )
-  set(CPACK_GENERATOR ${SPECIFIC_SYSTEM_PREFERED_CPACK_GENERATOR} )
-endif (NOT SPECIFIC_SYSTEM_PREFERED_CPACK_GENERATOR STREQUAL "" )
-
-if    (NOT MY_OS STREQUAL "" )
-  message(STATUS "---- Detect CPACK ----")
-  message(STATUS "Linux distribution :" ${MY_OS})
-  message(STATUS "OS version         :" ${LINUX_NAME})
-  message(STATUS "Generator          :" ${CPACK_GENERATOR})
-endif (NOT MY_OS STREQUAL "" )
-
+  # Debian / Ubuntu / Mint
+  if(OS_ID MATCHES "ubuntu|debian|mint" OR OS_LIKE MATCHES "debian|ubuntu")
+    set(CPACK_GENERATOR "DEB")
+    set(CPACK_DEBIAN_PACKAGE_SHLIBDEPS ON) # generate apt dependencies
+  # Fedora / RedHat / SuSE / Mageia
+  elseif(OS_ID MATCHES "fedora|rhel|centos|suse|mageia"
+          OR OS_LIKE MATCHES "rhel|fedora|suse")
+    set(CPACK_GENERATOR "RPM")
+  # if distrib not detected, then tarball .tar.gz
+  else()
+    set(CPACK_GENERATOR "TGZ")
+  endif()
+endif()
 
--- cmake/Version.cmake
+++ cmake/Version.cmake
@@ -16,14 +16,14 @@ find_package(Git QUIET)
 set(GIT_COMMIT_HASH "unknown")
 
 if(GIT_FOUND AND EXISTS "${CMAKE_CURRENT_SOURCE_DIR}/.git")
-    # run git rev-parse
-    execute_process(
-        COMMAND ${GIT_EXECUTABLE} rev-parse --short HEAD
-        WORKING_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}"
-        OUTPUT_VARIABLE GIT_COMMIT_HASH
-        OUTPUT_STRIP_TRAILING_WHITESPACE
-        ERROR_QUIET
-    )
+  # run git rev-parse
+  execute_process(
+      COMMAND ${GIT_EXECUTABLE} rev-parse --short HEAD
+      WORKING_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}"
+      OUTPUT_VARIABLE GIT_COMMIT_HASH
+      OUTPUT_STRIP_TRAILING_WHITESPACE
+      ERROR_QUIET
+  )
 endif()
 
 # Create version.h file in project binary directory
